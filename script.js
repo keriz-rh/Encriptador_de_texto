18ca8cc758da1b74b59877775f73a9d954939584
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-05-22 23:31:25 -0600

    Diseño de la aplicación con css y responsive design junto con la funcionalidad de encritar un texto

---
+++ script.js
@@ -0,0 +1,31 @@
+/*Funcion vinculada al botón del mismo nombre en HTML
+atraves del atributo onclick*/
+
+function encriptar() {
+
+    let texto = document.getElementById("texto").value;
+    let linkArco = document.getElementById("linkArco");
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    
+
+
+    let textocifrado = texto 
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imesr")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat")
+
+    if (texto.length != 0){
+        document.getElementById("texto").value = textocifrado;
+        tituloMensaje.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = ""; 
+        linkArco.src = "images/link_mano.png";
+    } else {
+        document.getElementById("linkArco").src = "images/link_apunta.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado."
+        parrafo.textContent = "Ingresa el texto que quieres desencriptar o desencritar"
+        alert("Debes ingresar algún texto.");
+    }
+    }
